5b119f42250f3323c1341d9c47a6f65cbca1e0bf
DVDNCL123 <164830789+DVDNCL123@users.noreply.github.com>
2025-05-17 12:38:44 +0700

    Create T7_no1.php

---
+++ LATIHAN/TUGAS 7/T7_no1.php
@@ -0,0 +1,24 @@
+<?php
+
+$jumlahRoda = 10;
+
+switch ($jumlahRoda) {
+    case 2:
+        echo "Kendaraannya: Motor atau Sepeda";
+        break;
+    case 3:
+        echo "Kendaraannya: Bajaj";
+        break;
+    case 4:
+        echo "Kendaraannya: Mobil";
+        break;
+    case 6:
+        echo "Kendaraannya: Truk Kecil ";
+        break;
+    case 10:
+        echo "Kendaraannya: Truk Besarrr kali bahh";
+        break;
+    default:
+        echo "Jumlah roda tidak diketahui nih bray";
+}
+?>
